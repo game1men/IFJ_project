c43e582cb9c35656e779368264b204550d20c3d4
nod3zer0 <xceska06@stud.fit.vutbr.cz>
2022-10-25 14:40:44 +0200

    Create enum for error codes and function for printing errors codes

--- IFJ-compiler/utils.c
+++ IFJ-compiler/utils.c
@@ -3,63 +3,105 @@
 const int DEFAULT_STRING_ALLOCATION = 20;
 const int EOS = '\0';
 
-String* InitString(int* exception) {
-    String *s = (String*)malloc(sizeof(String));
-    if(s == NULL) {
-        *exception = INVALID_ALLOCATION_EXCEPTION;
-        return NULL;
-    }
-    
-    s->length = 0;
-    s->allocated = DEFAULT_STRING_ALLOCATION;
-    s->chars = (char*)malloc(sizeof(char) * s->allocated);
+String *InitString(int *exception) {
+  String *s = (String *)malloc(sizeof(String));
+  if (s == NULL) {
+    *exception = INVALID_ALLOCATION_EXCEPTION;
+    return NULL;
+  }
 
-    if(s->chars == NULL) {
-        *exception = INVALID_ALLOCATION_EXCEPTION;
-        return NULL;
-    }
+  s->length = 0;
+  s->allocated = DEFAULT_STRING_ALLOCATION;
+  s->chars = (char *)malloc(sizeof(char) * s->allocated);
 
-    s->chars[0] = EOS;
+  if (s->chars == NULL) {
+    *exception = INVALID_ALLOCATION_EXCEPTION;
+    return NULL;
+  }
 
-    *exception = OK;
-    return s;
+  s->chars[0] = EOS;
+
+  *exception = OK;
+  return s;
 }
 
-int AppendChar(String* s, const char c) {
-    if(s == NULL) return NULL_POINTER_EXCEPTION;
+int AppendChar(String *s, const char c) {
+  if (s == NULL)
+    return NULL_POINTER_EXCEPTION;
 
-    // Reallocation
-    if(s->allocated <= s->length + 1) {
-        char* ptr = realloc(s->chars, s->allocated * 2);
+  // Reallocation
+  if (s->allocated <= s->length + 1) {
+    char *ptr = realloc(s->chars, s->allocated * 2);
 
-        if (ptr == NULL) return INVALID_ALLOCATION_EXCEPTION;
+    if (ptr == NULL)
+      return INVALID_ALLOCATION_EXCEPTION;
 
-        s->chars = ptr;
-    }
+    s->chars = ptr;
+  }
 
-    s->chars[s->length] = c;
-    s->length++;
-    s->chars[s->length] = EOS;
+  s->chars[s->length] = c;
+  s->length++;
+  s->chars[s->length] = EOS;
 
-    return OK;
+  return OK;
 }
 
-int RemoveLastChar(String* s) {
-    if(s == NULL) return NULL_POINTER_EXCEPTION;
+int RemoveLastChar(String *s) {
+  if (s == NULL)
+    return NULL_POINTER_EXCEPTION;
 
-    if(s->chars[0] == EOS || s->length == 0) return STRING_EMPTY_EXCEPTION;
+  if (s->chars[0] == EOS || s->length == 0)
+    return STRING_EMPTY_EXCEPTION;
 
-    s->length--;
-    s->chars[s->length] = EOS;
+  s->length--;
+  s->chars[s->length] = EOS;
 
-    return OK;
+  return OK;
 }
 
-int DisposeString(String* s) {
-    if(s == NULL) return NULL_POINTER_EXCEPTION;
+int DisposeString(String *s) {
+  if (s == NULL)
+    return NULL_POINTER_EXCEPTION;
 
-    free(s->chars);
-    free(s);
-    s = NULL;
-    return OK;
+  free(s->chars);
+  free(s);
+  s = NULL;
+  return OK;
+}
+
+int WriteErrorMessage(COMPILATION_ERROR_TYPE ce_t) {
+  switch (ce_t) {
+  case LEXICAL_ANALYSIS_ERROR:
+    fprintf(stderr, "Error in lexical analysis\n");
+    break;
+  case SYNTAKTIC_ANALYSIS_ERROR:
+    fprintf(stderr, "Error in syntactic analysis\n");
+    break;
+  case SEMANTIC_ERROR_FUNCTION_UNDEFINED_OR_REDEFINED:
+    fprintf(stderr, "Semantic error: undefined or redefined function\n");
+    break;
+  case SEMANTIC_ERROR_WRONG_ARGS_OR_RETURN_VALUE:
+    fprintf(stderr, "Semantic error: wrong amount of arguments or wrong type "
+                    "of return value\n");
+    break;
+  case SEMANTIC_ERROR_USE_OF_UNDEFINED_VARIABLE:
+    fprintf(stderr, "Semantic error: use of undefined variable\n");
+    break;
+  case SEMANTIC_ERROR_WRONG_NUMBER_OF_EXPRESSIONS:
+    fprintf(stderr,
+            "Semantic error: wrong number of expressions in return command\n");
+    break;
+  case SEMANTIC_ERROR_OF_TYPE_COMPATABILITY:
+    fprintf(stderr, "Semantic error: operation between uncompatible types\n");
+    break;
+  case OTHER_SEMANTIC_ERRORS:
+    fprintf(stderr, "Semantic error: other\n");
+    break;
+    break;
+  case INTERNAL_COMPILER_ERROR:
+    fprintf(stderr, "Internal compiler error\n");
+    break;
+  }
+
+  return ce_t;
 }
