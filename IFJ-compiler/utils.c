0b262905a65af53b69850d6a42ac7839a0a7b755
Petr Večeřa <petrvecera.personal@gmail.com>
2022-11-20 02:56:19 +0100

    Create compare function for var names in b-tree

--- IFJ-compiler/utils.c
+++ IFJ-compiler/utils.c
@@ -23,7 +23,6 @@ String* InitString(int* exception) {
 
     *exception = OK;
     return s;
-
 }
 
 int AppendChar(String* s, const char c) {
@@ -99,4 +98,63 @@ int WriteErrorMessage(COMPILATION_ERROR_TYPE ce_t) {
     }
 
     return ce_t;
+}
+
+/// @brief Performs comparasion between two variable names
+/// @param inputVarName Incoming variable name for comparision with node's variable name
+/// @param nodeVarName Node's variable name to compare to
+/// @return One when first occurrence ASCII value of input variable character is greater than in
+/// node. Zero, when variables are the same and minus one, when first occurrence ASCII value of
+/// input variable character is lower than in node. For different input's size as the node is result
+/// determined either in case of shorter input by last input's char compared with
+/// node's char on same index or in case of longer input by input's first character after
+/// the removal of node prefix. This char is  compared with node's last character. In both cases
+/// function returns one for input's greater value, otherwise minus one.
+int CompareVarNames(String* inputVarName, String* nodeVarName) {
+    if (nodeVarName == NULL || inputVarName == NULL) return NULL_POINTER_EXCEPTION;
+
+    // For inputs longer than node, take only one char more of node's length
+    int maxSufficentInputLength = min(inputVarName->length, nodeVarName->length + 1);
+
+    // Explicit difference check on shared indexes
+    for (int i = 0; i < maxSufficentInputLength; i++) {
+        int nodeIndex = min(i, nodeVarName->length - 1); // Prevent overflow
+        char nodeChar = nodeVarName->chars[nodeIndex];
+
+        char inputChar = inputVarName->chars[i];
+
+        int result = CompareChars(inputChar, nodeChar);
+        if (result != 0) return result;
+    }
+
+    // All chars were same on shared indexes, strings are same with same length
+    if (nodeVarName->length == inputVarName->length) return 0;
+
+    // Take last char from node for long input and same index char for short input
+    int nodeCompareIndex = min(nodeVarName->length - 1, maxSufficentInputLength - 1);
+
+    int result = CompareChars(inputVarName->chars[maxSufficentInputLength - 1],
+                              nodeVarName->chars[nodeCompareIndex]);
+
+    // Implicit to leftside with eqvivalence
+    if (result == 0) return -1;
+    return result;
+}
+
+/// @brief Compares ASCII value of two characters
+/// @param a first character
+/// @param b second character
+/// @return Minus one when first char has lower value than second one. Zero when are values the same. Otherwise one.
+int CompareChars(char a, char b) {
+    if (a == b) return 0;
+
+    if (a < b) {
+        return -1;
+    }
+
+    return 1;
+}
+
+int min(int a, int b) {
+    return a < b ? a : b;
 }
